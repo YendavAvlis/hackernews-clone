d6228d34b5c2461a18c7a756d6cab650d4875abf
Vadney Da Silva <31402270+YendavAvlis@users.noreply.github.com>
2025-08-14 17:08:26 +0200

    Update Story

--- components/Story.js
+++ components/Story.js
@@ -12,8 +12,8 @@ export default function Story(story){
             <a href="#/item?id=${story.id}">
                 ${story.comments_count} comments
             </a> |
-            <span class="favorite" data-story="${JSON.stringify(story)}">
-                 <ion-icon name="heart-outline"></ion-icon> ${story.isFavorite ? "Remove From Favorites" : "Add Favorites"}
+            <span class="favorite" data-story='${JSON.stringify(story)}'>
+                  ${story.isFavorite ? '<ion-icon name="heart"></ion-icon> Remove From Favorites' : '<ion-icon name="heart-outline"></ion-icon> Add Favorites'}
             </span>
         </div>
     </div>`
