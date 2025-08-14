21e30ed9838edbcce967267695f2a2f85c30060a
Vadney Da Silva <31402270+YendavAvlis@users.noreply.github.com>
2025-08-14 10:34:32 +0200

    Toggling Favorites

--- components/Story.js
+++ components/Story.js
@@ -12,8 +12,8 @@ export default function Story(story){
             <a href="#/item?id=${story.id}">
                 ${story.comments_count} comments
             </a> |
-            <span class="favorite">
-                <img class="heart" src="https://icon.now.sh/heart/ccc"> Add Favorites
+            <span class="favorite" data-story='${JSON.stringify(story)}'>
+                <img class="heart" src="https://icon.now.sh/heart/ccc"> ${story.isFavorite ? "Remove From Favorites" : "Add Favorites"}
             </span>
         </div>
     </div>`
--- pages/stories.js
+++ pages/stories.js
@@ -1,15 +1,30 @@
 import view from '../utils/view.js'
 import Story from '../components/Story.js'
 import baseUrl from '../utils/baseUrl.js';
+import store from '../store.js';
+import checkFavorite from '../utils/checkFavorite.js';
 
 export default async function Stories(path) {
+    const {favorites} = store.getState();
     const stories = await getStories(path);
 
     const hasStories = stories.length > 0;
 
     view.innerHTML = `<div>
-    ${hasStories ? stories.map((story, i) => Story({...story, index: i + 1})).join('') : 'No Stories'}
+    ${hasStories ? stories.map((story, i) => Story({...story, index: i + 1, isFavorite: checkFavorite(favorites, story)})).join('') : 'No Stories'}
     </div>`
+
+    document.querySelectorAll('.favorite').forEach(favoriteButton => {
+        favoriteButton.addEventListener('click', async function(){
+            const story = JSON.parse(this.dataset.story)
+            console.log(story)
+            const isFavorited = checkFavorite(favorites, story);
+            console.log(isFavorited)
+
+            store.dispatch({ type: isFavorited ? "REMOVE_FAVORITE" : "ADD_FAVORITE", payload: { favorite: story } })  
+            await Stories(path);
+        });
+    });
 }
 
 async function getStories(path) {
